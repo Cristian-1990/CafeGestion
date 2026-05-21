b46059a5d09a2f1d671251e45b31205b86330f8b
Cristian <crisalvfer14@gmail.com>
2026-05-21 17:18:08 +0200

    Añadido Storages Csv, Xml y Json

--- Config/Configuracion.cs
+++ Config/Configuracion.cs
@@ -1,10 +1,13 @@
 ﻿namespace CafeGestion.Config;
 /// <summary>
-/// Clase estatica que guarda la ruta y nombre de los ficheros.
+/// Clase estática que guarda la ruta y nombre de los ficheros.
 /// Evita tener valores mágicos dispersos por el código.
 /// </summary>
 public static class Configuracion
 {
     public static string DataFolder => "Data"; // Nombre de la carpeta dónde se guardaran los ficheros
     public static string CafesFile => Path.Combine(DataFolder, "cafes.json"); // Combina la carpeta "Data" con el nombre del archivo "cafes.json" para crear una ruta.
+    public static string CafesFileXml => Path.Combine(DataFolder, "cafes.xml");
+    public static string CafesFileCsv => Path.Combine(DataFolder, "cafes.csv");
+    public static string TipoStorage => "json"; //Cambia a csv o xml sin necesidad de compilar
 }
---
+++ Storage/StorageCsv/IStorageCsv.cs
@@ -0,0 +1,6 @@
+﻿using CafeGestion.Models;
+using CafeGestion.Storage.Common;
+
+namespace CafeGestion.Storage.StorageCsv;
+
+public interface IStorageCsv : IStorage<Producto> { }
